040f18f0bfd825ea833907f181f878839b983255
hamidazim321 <129533107+hamidazim321@users.noreply.github.com>
2023-07-26 21:56:41 +0500

    Add Final styles

--- src/Modules/ScoreURL.js
+++ src/Modules/ScoreURL.js
@@ -1,4 +1,4 @@
-const id = 'mwhOnEHWpvLPorUnYNZf';
+const id = 'YNH0lYCqMfebjYUpOW14';
 const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`;
 
 export default url;
